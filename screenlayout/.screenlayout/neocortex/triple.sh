4f180da9da10b73982098a373e8e2ba7c7005fee
Faris Chebib <faris@theluckybead.com>
2019-01-31 10:44:57 -0700

    Add neocortex layouts

---
+++ screenlayout/.screenlayout/neocortex/triple.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-1-1 --off --output DisplayPort-0 --off --output DVI-1-1 --off --output HDMI-1-1 --off --output DVI-I-2-3 --mode 1920x1080 --pos 904x1080 --rotate normal --output HDMI-0 --off --output DVI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
